ce219b18dfde53b8d69891d9c8c9d5897393df0c
raz <9876747+razpbrry@users.noreply.github.com>
2024-05-16 18:54:06 -0500

    Fix permission for extend cmd

--- Features/ExtendRoundTimeCommand.cs
+++ Features/ExtendRoundTimeCommand.cs
@@ -13,7 +13,7 @@ namespace cs2_rockthevote
     {
         [ConsoleCommand("css_extend", "Extends time for the current map")]
         [CommandHelper(minArgs: 1, usage: "<number of minutes to extend the map time ex. 30>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
-        [RequiresPermissions("@css/headadmins")]
+        [RequiresPermissions("@css/root")]
         public void OnExtendRoundTimeCommand(CCSPlayerController? player, CommandInfo commandInfo)
         {
             var newRoundTime = commandInfo.GetArg(1);
