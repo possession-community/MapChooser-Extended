b08e6a0b9e991a0f6b92c3fee8b9bd2b70163d64
Oz_Lin <lin@ozlin.info>
2024-09-02 16:37:14 +1000

    ignore spec from rtv/unrtv count

--- CrossCutting/ServerManager.cs
+++ CrossCutting/ServerManager.cs
@@ -5,19 +5,43 @@ namespace cs2_rockthevote
 {
     public static class ServerManager
     {
-        public static CCSPlayerController[] ValidPlayers(bool considerBots = false, bool ignoreSpecs = false)
+        private static bool _ignoreSpectators = true;
+
+        public static void SetIgnoreSpectators(bool ignore)
         {
-            //considerBots = true;
+            _ignoreSpectators = ignore;
+        }
+
+        public static bool GetIgnoreSpectators()
+        {
+            return _ignoreSpectators;
+        }
+
+        public static CCSPlayerController[] ValidPlayers(bool considerBots = false)
+        {
             return Utilities.GetPlayers()
-                .Where(x => x.ReallyValid(considerBots, ignoreSpecs))
-                .Where(x => !x.IsHLTV)
-                .Where(x => considerBots || !x.IsBot)
+                .Where(x => x.ReallyValid(considerBots, _ignoreSpectators))
                 .ToArray();
         }
 
-        public static int ValidPlayerCount(bool considerBots = false, bool ignoreSpecs = false)
+        public static int ValidPlayerCount(bool considerBots = false)
         {
-            return ValidPlayers(considerBots, ignoreSpecs).Length;
+            return ValidPlayers(considerBots).Length;
         }
+
+        //public static CCSPlayerController[] ValidPlayers(bool considerBots = false, bool ignoreSpecs = false)
+        //{
+        //    //considerBots = true;
+        //    return Utilities.GetPlayers()
+        //        .Where(x => x.ReallyValid(considerBots, ignoreSpecs))
+        //        .Where(x => !x.IsHLTV)
+        //        .Where(x => considerBots || !x.IsBot)
+        //        .ToArray();
+        //}
+
+        //public static int ValidPlayerCount(bool considerBots = false, bool ignoreSpecs = false)
+        //{
+        //    return ValidPlayers(considerBots, ignoreSpecs).Length;
+        //}
     }
 }
