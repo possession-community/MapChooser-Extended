45148de8a7f8f23979b5281379213b962967444b
Oz_Lin <lin@ozlin.info>
2024-09-13 23:52:12 +1000

    update translation phrases

--- Features/NominationListCommand.cs
+++ Features/NominationListCommand.cs
@@ -1,13 +1,20 @@
-﻿namespace cs2_rockthevote;
+﻿using CounterStrikeSharp.API;
+
+namespace cs2_rockthevote;
 
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Plugin;
 using CounterStrikeSharp.API.Modules.Commands;
+using cs2_rockthevote.Core;
 
 
+// public class NominationListCommand : IPluginDependency<Plugin, Config>
 public partial class Plugin
 {
+
     public char NewLine = '\u2029';
+    [ConsoleCommand("css_nomlist", "Show nominate list")]
     [ConsoleCommand("nomlist", "Show nominate list")]
     public void OnNomlist(CCSPlayerController? player, CommandInfo command)
     {
@@ -20,9 +27,9 @@ public partial class Plugin
 
         string Maplist = string.Join(NewLine.ToString(), Nomlist);
 
-        player.PrintToChat("*** Nominated Maps: ***");
+        player.PrintToChat(Localize("","nominate.nominate-list"));
         player.PrintToChat(Maplist);
-        player.PrintToChat("********************************");
+        //player.PrintToChat("********************************");
     }
 
 
