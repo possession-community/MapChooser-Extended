a07bbdb50b0445a9313078a97bc739fe0fb7a07d
Oz_Lin <lin@ozlin.info>
2024-09-20 20:31:45 +1000

    WIP: Round Limit Manager that deals with mp_maxrounds

---
+++ Core/RoundLimitManager.cs
@@ -0,0 +1,70 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Modules.Cvars;
+
+namespace cs2_rockthevote.Core
+{
+    public class RoundLimitManager : IPluginDependency<Plugin, Config>
+    {
+        private GameRules _gameRules;
+
+        private ConVar? _maxRounds; // move to RoundLimitManager.cs?
+
+        //private decimal TimeLimitValue => (decimal)(_timeLimit?.GetPrimitiveValue<float>() ?? 0F) * 60M;
+        private int RoundLimitValue => _maxRounds?.GetPrimitiveValue<int>() ?? 0;
+
+        public bool UnlimitedRound => RoundLimitValue <= 0;
+
+        public int RoundPlayed
+        {
+            get
+            {
+                if (_gameRules.WarmupRunning)
+                    return 0;
+
+                //return (decimal)(Server.CurrentTime - _gameRules.GameStartTime);
+                return -1; // todo
+            }
+        }
+
+        public int RoundsRemaining
+        {
+            get
+            {
+                if (UnlimitedRound || RoundPlayed > RoundLimitValue)
+                    return 0;
+
+                return RoundLimitValue - RoundPlayed;
+            }
+
+            set
+            {
+                _maxRounds.SetValue((int)value);
+            }
+        }
+
+        public RoundLimitManager(GameRules gameRules)
+        {
+            _gameRules = gameRules;
+        }
+
+        void LoadCvar()
+        {
+            _maxRounds = ConVar.Find("mp_maxrounds"); 
+        }
+
+        public void OnMapStart(string map)
+        {
+            LoadCvar();
+        }
+
+        public void OnLoad(Plugin plugin)
+        {
+            LoadCvar();
+        }
+
+        public void ExtendRound(int rounds)
+        {
+            // TODO: implement extending rounds
+        }
+    }
+}
